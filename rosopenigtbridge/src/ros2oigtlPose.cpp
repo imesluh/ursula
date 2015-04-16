646a7dc18db5bf9faa8b7bc91915f4c22c9f85ac
Johannes Gaa <jogaa@gmx.de>
2015-04-16 09:47:00 +0200

    Added non generic node

---
+++ rosopenigtbridge/src/ros2oigtlPose.cpp
@@ -0,0 +1,17 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+#include "ros2oigtlbridge.h"
+
+#include <geometry_msgs/Vector3.h>
+
+int main(int argc, char **argv)
+{
+
+  ros::init(argc, argv, "ros2openigtlink");
+  ros::NodeHandle n;
+
+
+
+  return 0;
+}
